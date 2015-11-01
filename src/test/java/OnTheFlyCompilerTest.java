a3cf0084aa278541f1bad22edbfba700c9420e24
gstraube <georg.straube@googlemail.com>
2015-11-01 20:12:48 +0100

    Add field of type BigInteger to dynamic class compilation test

--- src/test/java/OnTheFlyCompilerTest.java
+++ src/test/java/OnTheFlyCompilerTest.java
@@ -1,20 +1,21 @@
-import javassist.CannotCompileException;
-import javassist.ClassPool;
-import javassist.CtClass;
-import javassist.CtField;
+import javassist.*;
 import org.junit.Assert;
 import org.junit.Test;
+
+import java.math.BigInteger;
 
 import static org.hamcrest.CoreMatchers.is;
 
 public class OnTheFlyCompilerTest {
 
     @Test
-    public void class_with_instance_variables_can_be_created() throws CannotCompileException, NoSuchFieldException, IllegalAccessException, InstantiationException {
+    public void class_with_instance_variables_can_be_created() throws CannotCompileException, NoSuchFieldException, IllegalAccessException, InstantiationException, NotFoundException {
         ClassPool pool = ClassPool.getDefault();
         CtClass aClassCt = pool.makeClass("AClass");
         aClassCt.addField(CtField.make("public String aString = \"stringValue\";", aClassCt));
         aClassCt.addField(CtField.make("public int anInteger = 434343;", aClassCt));
+        String bigIntegerFieldSource = "public java.math.BigInteger aBigInteger = new java.math.BigInteger(\"5844\");";
+        aClassCt.addField(CtField.make(bigIntegerFieldSource, aClassCt));
         aClassCt.addField(CtField.make("public char aChar = 'l';", aClassCt));
         aClassCt.addField(CtField.make("public boolean aBoolean = true;", aClassCt));
 
@@ -23,6 +24,7 @@ public class OnTheFlyCompilerTest {
 
         Assert.assertThat(aClass.getField("aString").get(anObjectOfAClass), is("stringValue"));
         Assert.assertThat(aClass.getField("anInteger").get(anObjectOfAClass), is(434343));
+        Assert.assertThat(aClass.getField("aBigInteger").get(anObjectOfAClass), is(new BigInteger("5844")));
         Assert.assertThat(aClass.getField("aChar").get(anObjectOfAClass), is('l'));
         Assert.assertThat(aClass.getField("aBoolean").get(anObjectOfAClass), is(true));
     }
